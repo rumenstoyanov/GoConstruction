ceeb6a022eb624bcd06c485f2e6efeb911b700d0
Rumen Stoyanov <rumenxbox@hotmail.co.uk>
2020-08-31 23:01:27 +0100

    Reorganised Dtos into folders

---
+++ GoConstructionAPI/GoApi/Data/Dtos/Requests/SiteUpdateRequestDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoApi.Data.Dtos
+{
+    public class SiteUpdateRequestDto
+    {
+        [MaxLength(250)]
+        public string Title { get; set; }
+        [MaxLength(4000)]
+        public string Description { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
