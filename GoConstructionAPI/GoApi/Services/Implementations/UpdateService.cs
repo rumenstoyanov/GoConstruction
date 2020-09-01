40892273babd8254c79638df02fe2631f7b681a0
Rumen Stoyanov <rumenxbox@hotmail.co.uk>
2020-09-02 00:03:21 +0100

    Added update syntax assembly from Diff.
    And added test.

--- GoConstructionAPI/GoApi/Services/Implementations/UpdateService.cs
+++ GoConstructionAPI/GoApi/Services/Implementations/UpdateService.cs
@@ -2,12 +2,28 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace GoApi.Services.Implementations
 {
-    public class UpdateService
+    public class UpdateService : IUpdateService
     {
+        public string AssembleSyntaxFromDiff(Dictionary<string, string> diff)
+        {
+           
+            var sb = new StringBuilder();
+            foreach (var key in diff.Keys.SkipLast(1))
+            {
+                sb.Append($" updated the {key} to {diff[key]},");
+            }
+
+            var lastKey = diff.Keys.Last();
+            sb.Append($" updated the {lastKey} to {diff[lastKey]}.");
+            return sb.ToString();
+
+        }
+
         public Dictionary<string, string> Diff<T>(T preUpdate, T postUpdate) where T : class
         {
             var diffDict = new Dictionary<string, string>();
--- GoConstructionAPI/GoApi/Services/Interfaces/IUpdateService.cs
+++ GoConstructionAPI/GoApi/Services/Interfaces/IUpdateService.cs
@@ -8,5 +8,7 @@ namespace GoApi.Services.Interfaces
     public interface IUpdateService
     {
         Dictionary<string, string> Diff<T>(T preUpdate, T postUpdate) where T : class;
+
+        string AssembleSyntaxFromDiff(Dictionary<string, string> diff);
     }
 }
