9fcd55bb1c29a0896be9efe7b8b27ec28aae1a66
Rumen Stoyanov <rumenxbox@hotmail.co.uk>
2020-09-01 22:57:04 +0100

    Added Diff method to UpdateService

---
+++ GoConstructionAPI/GoApi/Services/Implementations/UpdateService.cs
@@ -0,0 +1,26 @@
+﻿using GoApi.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoApi.Services.Implementations
+{
+    public class UpdateService<T> : IUpdateService<T> where T : class
+    {
+        public Dictionary<string, string> Diff(T preUpdate, T postUpdate)
+        {
+            var diffDict = new Dictionary<string, string>();
+            var properties = typeof(T).GetProperties();
+            foreach (var pi in properties)
+            {
+                if (pi.GetValue(preUpdate) != pi.GetValue(postUpdate))
+                {
+                    diffDict.Add(pi.Name, pi.GetValue(postUpdate).ToString());
+                }
+            }
+            return diffDict;
+
+        }
+    }
+}
---
+++ GoConstructionAPI/GoApi/Services/Interfaces/IUpdateService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoApi.Services.Interfaces
+{
+    public interface IUpdateService<T> where T : class
+    {
+        Dictionary<string, string> Diff(T preUpdate, T postUpdate);
+    }
+}
